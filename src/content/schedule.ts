ac8e6c994ada746ee42d7a7e247b1b01e22a6b64
chris-aws-mano-it-dev <chris@manogames.com>
2026-07-21 14:36:31 +0000

    simplified schedule

--- src/content/schedule.ts
+++ src/content/schedule.ts
@@ -14,10 +14,8 @@ export const schedule: ScheduleItem[] = [
   { time: "08:00-08:30", title: "Arrival and Coffee", detail: "" },
   { time: "08:30-09:00", title: "Kickoff & Quests", detail: "" },
   { time: "09:00-10:00", title: "CIQ Challenge", detail: "Work in groups of 3-4" },
-  { time: "10:00-11:00", title: "Group presentations", detail: "Pen and Paper only" },
-  { time: "11:00-12:00", title: "Free Time / Meetings", detail: "" },
-  { time: "12:00-14:00", title: "Churrasco", detail: "" },
-  { time: "14:00-15:00", title: "Free Time / Meetings", detail: "" },
-  { time: "15:00-17:30", title: "Sports / Pool", detail: "" },
+  { time: "10:00-11:30", title: "Idea presentations", detail: "Pen and Paper only" },
+  { time: "11:30-15:00", title: "Churrasco", detail: "Also time for pool and meetings" },
+  { time: "15:00-17:30", title: "Sports", detail: "Last chance to win points" },
   { time: "17:30-18:00", title: "Closing", detail: "" },
 ];
